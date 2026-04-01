9ce60e7149ae8ee26549abbdb34ceb3716e79ec9
rishisharma056 <rs9490268@gmail.com>
2026-04-01 21:18:33 +0530

    Port Binding

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from dotenv import load_dotenv
+
 load_dotenv()  # load .env before anything else imports os.getenv()
 
 from app import create_app  # noqa: E402
@@ -6,4 +8,5 @@ from app import create_app  # noqa: E402
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
